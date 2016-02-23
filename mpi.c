1c16193df838fc2be6d87d2d8d73fa33f700f84d
Xochitl Xiomara <xochitl.morenobs@udlap.mx>
2016-02-23 15:43:32 -0600

    Added files via upload

---
+++ mpi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <iso646.h>
+int main(int argc, char const *argv[]){
+	int i=0;
+	int j=0;
+	float pi=0;
+for (i=1; i < 99999999; i=i+2){
+	if(i==1 or j==4) {
+		pi = pi + (4/(float)i);
+		j=2;
+	}
+	else {
+		pi= pi - (4/(float)i);
+		j = j + 2;
+	}
+}
+printf ("El valor de pi es: %1.52f", pi);
+
+	/* code */
+	return 0;
+}
